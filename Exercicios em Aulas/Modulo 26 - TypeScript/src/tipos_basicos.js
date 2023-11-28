e1983b8df79f15dbfe514f089e88f05ef1c10757
Gustavo Marques <gustavo-markes@hotmail.com>
2023-11-27 21:49:46 -0300

    Atualizando repositorio

---
+++ Exercicios em Aulas/Modulo 26 - TypeScript/src/tipos_basicos.js
@@ -0,0 +1,10 @@
+"use strict";
+let estaChovendo = false;
+estaChovendo = true;
+let idade = 25;
+let altura = 1.82;
+const nacionalidade = "Brasileira";
+const colegas = ["Gustavo", "Pedro", "Paulo"];
+const tecnoclogias = ["html", "css", "js"];
+const notas = [7, 8, 5, 2];
+notas.forEach((number) => console.log(number));
---
+++ Exercicios em Aulas/Modulo 26 - TypeScript/src/tipos_basicos.ts
@@ -0,0 +1,29 @@
+let estaChovendo: boolean = false;
+
+estaChovendo = true;
+
+let idade: number = 25;
+
+let altura: number = 1.82;
+
+const nacionalidade: string = "Brasileira";
+
+const colegas: string[] = ["Gustavo", "Pedro", "Paulo"];
+const tecnoclogias: Array<string> = ["html", "css", "js"];
+const notas: ReadonlyArray<number> = [7, 8, 5, 2];
+
+const lista: [nome: string, estaEstudando: boolean, idade: number] = [
+  "Gustavo",
+  true,
+  25,
+];
+
+let idadeDaAna: number | string = 25;
+idadeDaAna = "25 anos";
+
+let dadosDaApi: any;
+
+dadosDaApi = 10;
+dadosDaApi = true;
+dadosDaApi = [1, 2, 3];
+dadosDaApi = "String";
